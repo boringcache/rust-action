480f772cc8d139b5c67439763c999cc92595c562
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2026-02-18 10:58:04 +0000

    chore: release v1.0.1
    Bump default CLI version to v1.0.1.

--- dist/restore.js
+++ dist/restore.js
@@ -53,7 +53,7 @@ async function run() {
         const components = core.getInput('components');
         const profile = core.getInput('profile') || 'minimal';
         const rustVersion = await (0, utils_1.getRustVersion)(inputVersion, workingDir);
-        const cliVersion = core.getInput('cli-version') || 'v1.0.0';
+        const cliVersion = core.getInput('cli-version') || 'v1.0.1';
         // Set outputs
         core.setOutput('workspace', workspace);
         core.setOutput('rust-version', rustVersion);
