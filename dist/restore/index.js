e2cc80a4441a0c439f6dbd2ef5d4fff451867d96
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2026-02-20 09:30:19 +0000

    chore: release v1.1.1

--- dist/restore/index.js
+++ dist/restore/index.js
@@ -41630,7 +41630,7 @@ async function run() {
         const components = core.getInput('components');
         const profile = core.getInput('profile') || 'minimal';
         const rustVersion = await (0, utils_1.getRustVersion)(inputVersion, workingDir);
-        const cliVersion = core.getInput('cli-version') || 'v1.1.0';
+        const cliVersion = core.getInput('cli-version') || 'v1.1.1';
         // Set outputs
         core.setOutput('workspace', workspace);
         core.setOutput('rust-version', rustVersion);
